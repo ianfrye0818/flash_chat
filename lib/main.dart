ad888f9f2a7caa6ded2fbe435fe8b001e68dddf3
Ian <ian_frye@outlook.com>
2024-06-18 07:04:11 -0400

    registration working

---
+++ lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flash_chat/firebase_options.dart';
+import 'package:flash_chat/screens/chat_screen.dart';
+import 'package:flash_chat/screens/login_screen.dart';
+import 'package:flash_chat/screens/register_screen.dart';
+import 'package:flash_chat/screens/welcome_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  // await FirebaseAuth.instance.useAuthEmulator('localhost', 9099);
+  FirebaseAuth.instance.authStateChanges().listen((User? user) {
+    if (user == null) {
+      print('no user');
+    } else {
+      print(FirebaseAuth.instance.currentUser);
+    }
+  });
+  runApp(const FlashChat());
+}
+
+class FlashChat extends StatelessWidget {
+  const FlashChat({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    User? user = FirebaseAuth.instance.currentUser;
+    return MaterialApp(
+        theme: ThemeData.dark().copyWith(
+          textTheme: const TextTheme(
+            bodyMedium: TextStyle(color: Colors.black54),
+          ),
+        ),
+        debugShowCheckedModeBanner: false,
+        initialRoute: user == null ? '/' : '/chat',
+        routes: {
+          '/': (context) => const WelcomeScreen(),
+          '/login': (context) => const LoginScreen(),
+          '/register': (context) => const RegisterScreen(),
+          '/chat': (context) => const ChatScreen(),
+        });
+  }
+}
+
+class RegistrationScreen {}
